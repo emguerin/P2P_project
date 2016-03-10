586f15e7c12df43c0782293bda0836288070e2ce
jb lacour <dsijb@live.fr>
2016-03-10 11:38:37 +0100

    commit de Pair.java

--- src/Pair.java
+++ src/Pair.java
@@ -3,6 +3,7 @@ import java.net.Socket;
 import java.net.*;
 import java.util.Scanner;
 import java.io.IOException;
+import java.net.InetAddress;
 
 public class Pair implements Runnable {
 
@@ -16,14 +17,14 @@ public class Pair implements Runnable {
     private int port;
 
     //Constructeur
-    public Pair(int portWelcome, String adresseWelcome, int portHash, String adresseHash) {
+    public Pair(ClientHashServer clHash, ClientWelcomeServer clWelcome) {
 
-        //String myIp =   RECUP l'IP AUTOMATIQUEMENT
+        //récupération de l'adresse IP publique de la machine
+        String myIp = InetAddress.getLocalHost().getHostAddress();
 
         //Communication avec le HashServer
-        ClientHashServer clHash = new ClientHashServer(portHash, adresseHash);
-        System.out.println("Début de la communication avec le serveur de hash à l'adresse " + adresseHash);
-        this.hash = clHash.communiquer(); //METTRE IP EN PARAM 
+        System.out.println("Début de la communication avec le serveur de hash à l'adresse " + clHash.getAdresse());
+        this.hash = clHash.communiquer(); //METTRE myIp EN PARAM 
         System.out.println("Fin de la communication avec le serveur de hash\n");
 
         if(aht) {
@@ -31,8 +32,7 @@ public class Pair implements Runnable {
         }
 
         //Communication avec le WelcomeServer
-        ClientWelcomeServer clWelcome = new ClientWelcomeServer(portWelcome, adresseWelcome);
-        System.out.println("Début de la communication avec le serveur Welcome à l'adresse " + adresseWelcome);
+        System.out.println("Début de la communication avec le serveur Welcome à l'adresse " + clWelcome.getAdresse());
         this.tableRoutage = clWelcome.communiquer(); //METTRE IP ET HASH EN PARAM
         System.out.println("Fin de la communication avec le serveur Welcome\n");
 
@@ -55,6 +55,13 @@ public class Pair implements Runnable {
         this.port = port;
     }
 
+    //Accesseurs
+    public Map<Integer, Map<Integer, String>> getTableRoutage(){
+        return this.tableRoutage;
+    }
+    public int getPort(){
+        return this.port;
+    }
 
 
     //Partie Serveur
@@ -93,11 +100,14 @@ public class Pair implements Runnable {
         int    portHash       = Integer.parseInt(args[2]);
         String adresseHash    = args[3];
 
-        Pair pair = new Pair(portWelcome, adresseWelcome, portHash, adresseHash);
+        ClientHashServer clHash = new ClientHashServer(portHash, adresseHash);
+        ClientWelcomeServer clWelcome = new ClientWelcomeServer(portWelcome, adresseWelcome);
+
+        Pair pair = new Pair(clHash, clWelcome);
 
 
         //Création d'un thread d'écoute du MonitorServer
-        MoniteurComm moniteurComm = new MoniteurComm(this.port, this.tableRoutage);
+        MoniteurComm moniteurComm = new MoniteurComm(pair.getPort(), pair.getTableRoutage());
         Thread threadMC = new Thread(moniteurComm);
         threadMC.start();
 
@@ -106,14 +116,13 @@ public class Pair implements Runnable {
         threadServ.start();
 
 
-
         //Gestion du côté client du pair
         Scanner sc = new Scanner(System.in);
         String ip;
         int h;
         ClientPair clPair;
         while(true) {
-            System.out.println("Quelle pair souhaitez-vous contacter (donnez une adresse IP) ?");
+            System.out.println("Quelle pair souhaitez-vous contacter (donnez un hash) ?");
             ip = sc.nextLine();
             //récupération du hash du pair destinataire
             h = clHash.communiquer(ip); //il faudra mettre l'ip en paramètre dans la classe ClientHashServer!!!!!!!!!!!
