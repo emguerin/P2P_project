e08a7bc6f36fb3fce8a050ad85b71d2174735290
Rémi Grison <rl.grison@gmail.com>
2016-03-10 11:33:14 +0100

    ajout lignes pour gérer intégration dans réseau P2P (pas encore terminé)

--- src/ClientWelcomeServer.java
+++ src/ClientWelcomeServer.java
@@ -2,14 +2,19 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 import java.util.Map;
+import java.util.HashMap;
+
 
 public class ClientWelcomeServer {
-    protected int    port;
-    protected String adresseServeur;
+    private int    port;
+    private String adresseServeur;
+    private int    hash;
+    final static int LE_PORT = 8001;
 
-    public ClientWelcomeServer(int port, String adresseServeur) {
+    public ClientWelcomeServer(int port, String adresseServeur, int hash) {
         this.port           = port;
         this.adresseServeur = adresseServeur;
+        this.hash           = hash;
     }
 
     public Map<Integer, Map<Integer, String>> communiquer() {
@@ -37,7 +42,7 @@ public class ClientWelcomeServer {
 
         String msg;
         String entreeLue;
-        
+
         msg = sc.nextLine();
 
         // On écrit sur la sortie
@@ -54,9 +59,42 @@ public class ClientWelcomeServer {
         /*
         * Si on recoit le msg yaf, alors on dit que succ est lui-même
         *
-        * Sinon, on recoit l'IP du successeur comme message (au lieu de yaf) et on doit la conserver pour créer la 
-        * table de routage du pair 
+        * Sinon, on recoit l'IP du successeur comme message (au lieu de yaf) et on doit la conserver pour créer la
+        * table de routage du pair
         */
+
+        // Instanciation de la table de routage
+        Map<Integer, Map<Integer, String>> tr = new HashMap<Integer, Map<Integer, String>>();
+        if (entree.readLine().equals("yaf")) {
+            Map<Integer, String> soi = new HashMap<Integer, String>();
+
+            /*
+             * On ajoute la 2ème partie de la ligne dans la map
+             */
+            try {
+                soi.put(this.hash, InetAddress.getLocalHost().getHostAddress());
+            } catch (UnknownHostException e) {
+                System.out.println(e.getMessage());
+            }
+
+            tr.put(this.hash, soi);
+        } else {
+            /*
+             * On reçoit une IP d'une personne du réseau
+             * on doit le communiquer et lui dire qu'on veut rentrer sur
+             * le serveur avec un message de la forme : "TRUC"
+             * ensuite on aura la table de routage
+             */
+            String ipSucc = entree.readLine();
+            ClientHashServer chs = new ClientHashServer(LE_PORT, this.adresseServeur);
+            // Passer l'IP voulue au HashServer
+            int hashSucc = chs.communiquer();
+            Map<Integer, String> succ = new HashMap<Integer, String>();
+            succ.put(hashSucc, ipSucc);
+            tr.put(this.hash, succ);
+        }
+
+        return tr;
     }
 
     public Socket etablirConnexion() {
