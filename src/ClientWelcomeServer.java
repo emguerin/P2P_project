767d8fd5c8efc80ce8d8df744fd01bd22539aa0a
Rémi Grison <rl.grison@gmail.com>
2016-03-15 18:44:07 +0100

    Correction ClientWelcomeServer, gestion des exceptions modifications des try {} par des try (objets qui seront fermés automatiquement) {}

--- src/ClientWelcomeServer.java
+++ src/ClientWelcomeServer.java
@@ -21,117 +21,108 @@ public class ClientWelcomeServer {
         return this.adresseServeur;
     }
 
-    public ArrayList<LigneRoutage> communiquer(String notreIP, int hash) {
-        Socket sock = etablirConnexion();
-
-        /*
-         * Mise en place des flux et des buffers pour écrire/recevoir les
-         * messages + facilement
-         */
-        InputStream  fluxEntree = null;
-        OutputStream fluxSortie = null;
-
-        try {
-            fluxEntree = sock.getInputStream();
-            fluxSortie = sock.getOutputStream();
-        } catch (IOException ioe) {
-            System.err.println(ioe.getMessage());
-        }
-
-        BufferedReader entree = new BufferedReader(new InputStreamReader(fluxEntree));
-        PrintWriter    sortie = new PrintWriter(fluxSortie, true);
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Demandez à entrer dans le réseau P2P en envoyant un message de la forme yo:hash(ip):ip");
-
-        String msg;
-        String entreeLue;
-
-        msg = sc.nextLine();
-
-        // On écrit sur la sortie
-        sortie.println(msg);
-
-        entreeLue = this.lireMessage(entree);
-
-        System.out.println("Message retourné par le serveur Welcome : " + entreeLue);
-
-        if (entreeLue.equals("wrq")) {
-            System.err.println("Mauvaise requête");
-            return null;
-        }
-
-        /*
-         * Si on recoit le msg yaf, alors on dit que notre successeur est
-         * nous-même
-         *
-         * Sinon, on recoit l'IP de quelqu'un d'autre du réseau
-         */
-
+    /*
+     * Le type de retour est List et non ArrayList car c'est l'interface
+     * C'est une bonne pratique à adopter et si on veut changer de classe
+     * (LinkedList par ex.) ce sera plus facile
+     */
+    public List<LigneRoutage> communiquer(String notreIP, int hash) {
         // Instanciation de la table de routage
-        ArrayList<LigneRoutage> tr = new ArrayList<LigneRoutage>();
+        List<LigneRoutage> tr = new ArrayList<LigneRoutage>();
 
-        String entr = null;
-        try {
-            entr = entree.readLine();
+        try (
+        /*
+         * Tout ce qui est dans ces parenthèses sera fermé automatiquement
+         * à la fin du bloc try
+         */
+        Socket sock = etablirConnexion();
+        BufferedReader entree = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        PrintWriter    sortie = new PrintWriter(sock.getOutputStream(), true);
+        ){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Demandez à entrer dans le réseau P2P en envoyant un message de la forme yo:hash(ip):ip");
+
+            String msg;
+            String entreeLue;
+
+            msg = sc.nextLine();
+
+            // On écrit sur la sortie
+            sortie.println(msg);
+
+            // Cette méthode gère le try / catch
+            entreeLue = this.lireMessage(entree);
+
+            System.out.println("Message retourné par le serveur Welcome : " + entreeLue);
+
+            if (entreeLue.equals("wrq")) {
+                System.err.println("Mauvaise requête");
+                sc.close();
+                return null;
+            }
+
+            /*
+             * Si on recoit le msg yaf, alors on dit que notre successeur est
+             * nous-même
+             *
+             * Sinon, on recoit l'IP de quelqu'un d'autre du réseau
+             */
+            String entr = null;
+            entr = this.lireMessage(entree);
+
+            if (entr.equals("yaf")) {
+                tr.add(new LigneRoutage(hash, hash, notreIP));
+            } else {
+                /*
+                 * On reçoit une IP d'une personne du réseau
+                 * on doit le communiquer et lui dire qu'on veut rentrer sur
+                 * le serveur avec un message de la forme : yo:hash(soi):ip(soi)
+                 * ensuite on aura la table de routage. On nous envoie d'abord
+                 * la ligne concernant le prédecesseur puis la ligne de notre
+                 * successeur
+                 */
+                String ipMembre = entr;
+
+                /*
+                 * On récupère les infos nécessaires sur notre successeur et
+                 * notre prédecesseur au membre dont on a l'IP
+                 */
+                LigneRoutage succ = null;
+                LigneRoutage pred = null;
+
+                // succ et pred sont instanciés dans la fonctions
+                this.recupererPairs(succ, pred, ipMembre);
+                tr.add(pred);
+                tr.add(succ);
+            }
+            sc.close();
+        } catch (UnknownHostException uhe) {
+            System.err.println(uhe.getMessage());
         } catch (IOException ioe) {
             System.err.println(ioe.getMessage());
-        }    
-
-        if (entr.equals("yaf")) {
-            tr.add(new LigneRoutage(hash, hash, notreIP));
-        } else {
-            /*
-             * On reçoit une IP d'une personne du réseau
-             * on doit le communiquer et lui dire qu'on veut rentrer sur
-             * le serveur avec un message de la forme : yo:hash(soi):ip(soi)
-             * ensuite on aura la table de routage. On nous envoie d'abord
-             * la ligne concernant le prédecesseur puis la ligne de notre
-             * successeur
-             */
-            String ipMembre = entr;       
-
-            /*
-             * On récupère les infos nécessaires sur notre successeur et
-             * notre prédecesseur au membre dont on a l'IP
-             */
-            LigneRoutage succ = null;
-            LigneRoutage pred = null;
-
-            // succ et pred sont instanciés dans la fonctions
-            this.recupererPairs(succ, pred, ipMembre);
-            tr.add(pred);
-            tr.add(succ);
         }
-
+        
         return tr;
     }
 
 
     private void recupererPairs(LigneRoutage succ, LigneRoutage pred, String ip) {
-        Socket sock;
-        try {
-            sock = new Socket(ip, 2016); // port 2016
 
-            InputStream  fluxEntree = null;
-            OutputStream fluxSortie = null;
-
-            fluxEntree = sock.getInputStream();
-            fluxSortie = sock.getOutputStream();
-
-            BufferedReader entree = new BufferedReader(new InputStreamReader(fluxEntree));
-            PrintWriter    sortie = new PrintWriter(fluxSortie, true);
-
+        try (
+        Socket sock = new Socket(ip, 2016);
+        BufferedReader entree = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        PrintWriter    sortie = new PrintWriter(sock.getOutputStream(), true);
+        ) {
             /*
              * La communication. On nous renverra :
              *   - pour le prédécesseur : hash_pred:mon_hash:mon_ip
              *   - pour le successeur   : hash_moi:hash_lui:ip_lui
              *
              * On récupère
-             */  
-            String predecesseur = entree.readLine();  
-            String successeur = entree.readLine();
-           
+             */
+            String predecesseur = entree.readLine();
+            String successeur   = entree.readLine();
+
             pred = new LigneRoutage(predecesseur);
             succ = new LigneRoutage(successeur);
 
